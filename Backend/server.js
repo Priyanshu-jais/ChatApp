0728840fbff343798d5e42fed30345d3254bcfdc
PRIYANSHU JAISWAL <priyanshujaiswal184@gmail.com>
2024-07-22 20:54:06 +0530

    deploying time

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -13,6 +14,8 @@ import connectToMongoDb from "./db/connectToMongoDb.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); //to parse the incoming requests with json payloads(from req.body)
@@ -22,11 +25,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-// app.get("/", (req, res) => {
-//   //root route https://localhost:5000/
-//   res.send("Hello world with MERN");
-// })
+app.get("*",(req,res) => {
+  res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
+
+
 
 server.listen(PORT, () => {
   connectToMongoDb();
